acfc39a7179f880051fa91be128e813075365060
chenanguo <cag2050@163.com>
2019-12-02 19:50:12 +0800

    project init

---
+++ src/main/java/com/example/alibabasentinelratelimiting/AlibabaSentinelRateLimitingApplication.java
@@ -0,0 +1,13 @@
+package com.example.alibabasentinelratelimiting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlibabaSentinelRateLimitingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlibabaSentinelRateLimitingApplication.class, args);
+    }
+
+}
